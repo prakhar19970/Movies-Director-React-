6fda5e60f7219af3416c50175d078bee6782db37
prakhar19970 <prakharshukla20160@gmail.com>
2020-01-20 22:50:32 +0530

    new routes set

--- src/App.js
+++ src/App.js
@@ -1,27 +1,45 @@
 import React, { Component } from 'react';
 import Showmovies from './components/Moviespage/Showmovies'
-// import Showdirectors from './components/Directorpage/Showdirectors'
-import homepage from './components/main/homepage'
+import Showdirectors from './components/Directorpage/Showdirectors'
+import Homepage from './components/main/homepage'
+import Addnewmovie from './components/Moviespage/Addnewmovie'
+import Addnewdirector from './components/Directorpage/Addnewdirector'
+import Singledirector from './components/Directorpage/Singledirector'
+import Singlemovies from './components/Moviespage/Singlemovie'
 import './App.css'
-import { Route, BrowserRouter, Switch, withRouter } from "react-router-dom";
-
+import { Route, BrowserRouter, Switch, Link, withRouter } from "react-router-dom";
+import Updatemovie from './components/Moviespage/Updatemovie';
+import Updatedirector from './components/Directorpage/Updatedirector';
 
 class App extends Component {
+  componentDidMount() {
+  }
+
   render() {
     return (
       <BrowserRouter>
         <div className="App">
-          <homepage.Header />
-          <homepage.Body />
+          <header className="App-header" >
+            <h1>FlixInfo</h1>
+          </header>
           <Switch>
             <Route path="/movies" component={Showmovies} exact />
+            <Route path='/movies/add' component={Addnewmovie} exact />
+            <Route path='/home' component={Homepage} exact />
+            <Route path="/directors" component={Showdirectors} exact />
+            <Route path='/directors/add' component={Addnewdirector} exact />
+            <Route path='/directors/:id/update' component={Updatedirector} exact />
+            <Route path="/directors/:id" component={Singledirector} exact />
+            <Route path='/movies/:id/update' component={Updatemovie} exact />
+            <Route path="/movies/:id" component={Singlemovies} exact />
 
           </Switch>
         </div>
-      </BrowserRouter>
+      </BrowserRouter >
     );
   }
 }
 
 export default App;
-            /* <Route path="/directors" component={Showdirectors} exact /> */
+            /*
+/* <Route path='/movies/add' component={Addnewmovie} exact /> */
--- src/components/main/homepage.js
+++ src/components/main/homepage.js
@@ -1,32 +1,27 @@
 
-import React from 'react';
+import React, { Component } from 'react'
 import { Link } from 'react-router-dom'
 
-function Header() {
-    return (
 
-        <header className="App-header" >
-            <h1>FlixInfo</h1>
+class Homepage extends Component {
 
-        </header>
-    )
+    render() {
+        return (
+            <div className="movie-link" >
+                <Link to="/movies">
+                    <button className='movies-button' type="button">
+                        Movies
+                    </button>
+                </Link>
+                <Link to="/directors">
+                    <button className='movies-button' type="button">
+                        Directors
+                    </button>
+                </Link>
+            </div>
+        )
+    }
 
 }
-
-function Body() {
-    return (
-        <div className="movie-link">
-            <Link to="/movies">
-                <button className='movies-button' onClick={disappear} type="button">
-                    Movies
-                </button>
-            </Link>
-        </div>
-    )
-}
-function disappear() {
-    console.log('hello');
-    document.getElementsByClassName('movie-link')[0].style.display = 'none';
-}
-export default { Header, Body };
+export default Homepage;
 
